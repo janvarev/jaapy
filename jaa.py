1469737fb3bda5d3e60dae311a66c715b90832f9
janvarev <janvarev@gmail.com>
2022-02-05 15:40:17 +0300

    v1.5 - "options" default path is calculated as self.jaaRootFolder+os.path.sep+"options" (depends on current root folder/jaa.py folder)

--- jaa.py
+++ jaa.py
@@ -46,14 +46,15 @@ import os
 import traceback
 import json
 
-version = "1.4"
+version = "1.5"
 
 class JaaCore:
     def __init__(self):
-        self.jaaOptionsPath = "options"
+
         self.jaaPluginPrefix = "plugin_"
         self.jaaVersion = version
         self.jaaRootFolder = os.path.dirname(__file__)
+        self.jaaOptionsPath = self.jaaRootFolder+os.path.sep+"options"
         print("JAA.PY v{0} class created!".format(version))
 
     # ------------- plugins -----------------
